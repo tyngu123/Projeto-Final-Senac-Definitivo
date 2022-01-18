8e3fc71109ec694d6dab01bf9644493e3337662d
danrjntc <danilodamaseno16@gmail.com>
2022-01-17 21:24:38 -0300

    17.01

--- frontend/src/app/pages/vagas-ativas/vagas-ativas.page.ts
+++ frontend/src/app/pages/vagas-ativas/vagas-ativas.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { Vagas } from 'src/app/interfaces/vagas';
 import { VagasService } from 'src/app/services/vagas.service';
@@ -14,7 +14,8 @@ export class VagasAtivasPage implements OnInit {
   public vagas = new Array<Vagas>();
   private vagasSubscription: Subscription;
   
-  constructor(private vagasService: VagasService,private toastCtrl: ToastController) {
+  constructor(private vagasService: VagasService,private toastCtrl: ToastController,
+    private alertCtrl:AlertController) {
     this.vagasSubscription = this.vagasService.getVagas().subscribe(data => {
       this.vagas = data;
     });
@@ -29,6 +30,7 @@ export class VagasAtivasPage implements OnInit {
   async deleteVaga(id: string) {
     try {
       this.vagasService.deleteVaga(id);
+
     } catch (error) {
       this.presentToast('Erro ao tentar deletar');
     }
@@ -37,6 +39,56 @@ export class VagasAtivasPage implements OnInit {
   async presentToast(message: string) {
     const toast = await this.toastCtrl.create({ message, duration: 2000 });
     toast.present();
+  }
+
+  async ShowEditar(){
+    const myAlert = await this.alertCtrl.create({
+      header: 'EDITAR',
+     // subHeader: 'SUB HEADER',
+     // message:'MESSAGE',
+      
+      inputs:[
+        {
+          name:'nome',
+          placeholder:'Vaga de trabalho',
+
+        },
+
+        {
+           name:'estado',
+           placeholder:'Estado',
+           type:'text',
+
+        },
+        {
+         
+          name:'jornada',
+          placeholder:'Jornada de trabalho',
+          type:'text',
+
+       },
+       {
+        name:'salario',
+        placeholder:'Salário',
+        type:'number',
+     },
+    ],
+
+      buttons:[
+        {
+          text:'Cancelar',
+          handler: ()=>{console.log("Não editou os dados")}
+        },
+        {
+          text:'Confirmar',
+          handler: ()=>{console.log("Dados editados")}
+        },
+
+      ]
+
+    });
+
+    myAlert.present();
   }
 
 }
